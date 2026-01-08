f72832b9f6a65228d80d75fd0eba32261ce5a88d
unknown <bssem-f22-118@superior.edu.pk>
2026-01-08 11:27:27 -0800

    hey

--- src/app/models/Store.js
+++ src/app/models/Store.js
@@ -102,15 +102,15 @@ storeSchema.index({ verificationStatus: 1 });
 storeSchema.index({ status: 1 });
 
 // Password hashing middleware
-storeSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+storeSchema.pre("save", async function () {
+  if (!this.isModified("password")) return ;
 
   try {
     const salt = await bcrypt.genSalt(12);
     this.password = await bcrypt.hash(this.password, salt);
-    next();
+   
   } catch (error) {
-    next(error);
+    throw error;
   }
 });
 
